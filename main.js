7bc2f5068829c04aacde6d33bf0165cecfde6001
unwantet <darkblezer59@gmail.com>
2023-11-14 21:00:24 +0500

    add new hw

---
+++ main.js
@@ -0,0 +1,184 @@
+// 1
+
+// let a = +prompt("Son kiriting");
+// if (a > 0) {
+//     alert(a+1);
+// }
+// else{
+//     alert(a);
+// }
+
+///////////////////////
+
+//  2
+
+// let a = +prompt("Son kiriting");
+// if (a > 0) {
+//     alert(a+1);
+// }
+
+// else{
+//     alert(a-2);
+// }
+
+///////////////////////
+
+// 3
+
+// let a = +prompt("Son kiriting");
+// if (a > 0) {
+//     alert(a+1);
+// }
+
+// else if( a == 0){
+//     alert(10);
+// }
+// else{
+//     alert(a-2);
+// }
+
+///////////////////////
+
+//6
+
+// let a = +prompt("1-Son kiriting");
+// let b = +prompt("2-Son kiriting");
+// if (a > b) {
+//     alert("1-son katta");
+// }
+// else{
+//     alert("2-son katta");
+// }
+
+///////////////////////
+
+// 7
+
+// let a = +prompt("1-Son kiriting");
+// let b = +prompt("2-Son kiriting");
+// if (a < b) {
+//     alert("2-son kichig");
+// }
+// else{
+//     alert("1-son kichig");
+// }
+
+///////////////////////
+
+// 8
+
+// let a = +prompt("1-Son kiriting");
+// let b = +prompt("2-Son kiriting");
+// let c = " , "
+// if (a > b) {
+//     alert(a+c+b);
+// }
+// else{
+//     alert(b+c+a);
+// }
+
+////////////////////////////
+
+// 9
+
+// let a = +prompt("1-Son kiriting");
+// let b = +prompt("2-Son kiriting");
+// let c = "a kichig = ";
+// let d = "b katta = ";
+// let w = " , "
+// if (a > b) {
+//     alert(c+b +w+ d+a);
+// }
+// else{
+
+//     alert( c+a +w+ d+b);
+// }
+
+/////////////////////////////////
+
+// 10 
+
+// let a = +prompt("1-Son kiriting");
+// let b = +prompt("2-Son kiriting");
+// let c = " , ";
+
+// if (a == b) {
+//     alert(a +c+ b);
+// }
+// else{
+//     alert(a+b);
+// }
+
+/////////////////////////
+
+// 11
+
+// let a = +prompt("1-Son kiriting");
+// let b = +prompt("2-Son kiriting");
+// let c = " , ";
+
+// if (a == b) {
+//     alert(a +c+ b);
+// }
+// else if(a>b){
+//     alert(a);
+// }
+// else{
+//     alert(b)
+// }
+
+///////////////////////////////
+
+// 12
+
+// let a = +prompt("1-Son kiriting");
+// let b = +prompt("2-Son kiriting");
+// let c = +prompt("3-Son kiriting");
+
+// if (c>a || b>a) {
+//     alert(a);
+// }
+// else if ( b<c || b<a  ){
+//     alert(b);
+// }
+// else if(c<b || c<a){
+//     alert(c);
+// }
+
+
+
+
+// Boolean
+
+//1
+
+// let a = +prompt("son...")
+// let b = a>0 ? true : false;
+// alert(b)
+
+//2
+
+// let a = +prompt("son...")
+// let b = a%2==1 ? true : false;
+// alert(b)
+
+//3
+
+// let a = +prompt("son...")
+// let b = a%2==0 ? true : false;
+// alert(b)
+
+//4
+
+// let a = +prompt("1-son...");
+// let b= +prompt("2-son...");
+// let c = a>2 && b<=3  ? true : false;
+// alert(b)
+
+//5
+
+// let a = +prompt("1-son...");
+// let b= +prompt("2-son...");
+// let c = a>=0 || b<-2  ? true : false;
+// alert(b)
+
